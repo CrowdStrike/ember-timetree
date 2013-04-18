2f03d8206c7cc7e6a6edcfd36667b2217229aecd
Peter Wagenet <peter.wagenet@gmail.com>
2013-04-18 13:38:09 -0700

    Add to CONTRIBUTORS

--- packages/ember-timetree/lib/core.js
+++ packages/ember-timetree/lib/core.js
@@ -1,4 +1,4 @@
-// Copyright (C) 2013 CrowdStrike, Inc.
+// Copyright (C) 2013 CrowdStrike, Inc. and contributors
 // This file is subject to the terms and conditions of the BSD License.
 // See the file LICENSE in the main directory for details
 

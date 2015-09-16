afa33ef23fdec772208d3cc703a1e319b2620a17
John Kurkowski <john.kurkowski@crowdstrike.com>
2015-09-15 20:42:22 -0700

    Snippet to reproduce #19

---
+++ tests/dummy/app/controllers/index.js
@@ -0,0 +1,46 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+  events: [
+    {
+      label: "part 1",
+      start: 0,
+      end: 2
+    },
+    {
+      label: "part 2",
+      start: 2,
+      end: 4
+    },
+    {
+      label: "part 3",
+      start: 4,
+      end: 6
+    }
+  ],
+
+  actions: {
+    dummyAction: function(){
+      this.set('events', [
+        {
+          label: "part 1",
+          start: 0,
+          end: 1.9
+        },
+        {
+          label: "part 2",
+          start: 1.9,
+          end: 3.7
+        },
+        {
+          label: "part 3",
+          start: 3.7,
+          end: 5.5
+        }
+      ]);
+    }
+  }
+
+});
+
